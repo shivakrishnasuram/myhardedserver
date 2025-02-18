9b299f6d388bf3f4b2432c9cf28b92dafea7d118
shivakrishnasuram <suramshivakrishnasuram@gmail.com>
2025-02-18 09:36:23 +0530

    initials

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http =require("http")
 const server =http.createServer((req,res)=>{
-    res.write("this is my new response")
+    res.write("pole adhiri pole !!!!")
     res.end("response emded here")
 })
 server.listen(4000,()=>{
